8aab30adcbf9431f9e8d962687277ad246a8e619
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2021-12-11 23:19:40 -0500

    Optimize

--- 2015/Day 12/puzzle23.py
+++ 2015/Day 12/puzzle23.py
@@ -1,8 +1,15 @@
 import re
+import json
+
+def hook(obj):
+    if "red" in obj.values(): return{}
+    else: return obj
 
 with open("input.txt", 'r', encoding='utf-8') as f:
-    input = f.read()
-    
+    lines = f.read()
+
+input = str(json.loads(lines, object_hook=hook))
+
 num = [int(x) for x in re.findall(r'[-\d]+', input)]
     
 print(sum(num))
--- 2015/Day 12/puzzle24.py
+++ 2015/Day 12/puzzle24.py
@@ -4,8 +4,10 @@ with open("input.txt", 'r', encoding='utf-8') as f:
     input = f.read()
     
 total = sum([int(x) for x in re.findall(r'[-\d]+', input)])
-reds = [x for x in re.findall(r'{[^{]*?:\"red\".+?}', input)]
-reds = ''.join(reds)
-red_total = sum([int(x) for x in re.findall(r'[-\d]+', reds)])
+red_total = 0
+while len([x for x in re.findall(r'{[^{]*?:\"red\".+?}', input)]) > 0:
+    reds = [x for x in re.findall(r'{[^{]*?:\"red\".+?}', input)]
+    reds = ''.join(reds)
+    red_total += sum([int(x) for x in re.findall(r'[-\d]+', reds)])
     
-print(total-red_total)
+print(total - red_total)
