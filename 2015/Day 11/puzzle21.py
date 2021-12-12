48fe37667329cacbb6666f4e9c0d3ca0b6bf694f
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2021-12-11 22:21:18 -0500

    Day 11

---
+++ 2015/Day 11/puzzle21.py
@@ -0,0 +1,34 @@
+from numpy import diff
+from itertools import groupby
+
+input = 'cqjxjnds'
+bad_chars = ['i', 'o', 'l']
+
+next_alpha = lambda s: chr((ord(s)+1 - 97) % 26 + 97)
+check_bad_chars = lambda s: not any([c in s for c in bad_chars])
+
+def check_pairs(s):
+    o = [1 if x==y else 0 for x, y in zip(s[1:], s[:-1])]
+    t = groupby(o)
+    
+    return len([x for x, y in t if x==1]) > 1    
+
+def check_increasing(s):
+    o = diff([ord(a) for a in s])
+    p = ''.join(['1' if i == 1 else '0' for i in o ])
+
+    return '11' in p
+    
+
+def inc_char(s, i):
+    t = list(s)
+    t[i] = next_alpha(t[i])
+    return ''.join(t)
+    
+while not check_bad_chars(input) or not check_pairs(input) or not check_increasing(input):
+    for i in range(-1,-len(input),-1):
+        input = inc_char(input, i)
+        if not input[i] == 'a':
+            break
+        
+print(input)
---
+++ 2021/Day 11/puzzle21.py
@@ -0,0 +1,31 @@
+iter = 100
+flash = 0
+
+with open("input.txt", 'r', encoding='utf-8') as f:
+    octo = [[int(x) for x in y.strip()] for y in f]
+
+def increase_energy(x,y):
+    global flash
+    global flash_this_step
+    
+    octo[x][y] += 1
+    if octo[x][y] == 10:
+        l = [x,y]
+        flash_this_step.append(l) if l not in flash_this_step else flash_this_step
+        x_mod = range(max(x-1, 0), min(x+2, len(octo[0])))
+        y_mod = range(max(y-1, 0), min(y+2, len(octo)))
+        for i in x_mod:
+            for j in y_mod:
+                if not [i, j] == [x, y]:
+                    increase_energy(i, j)                      
+
+for n in range(iter):
+    flash_this_step = []
+    for x in range(len(octo[0])):
+        for y in range(len(octo)):
+            increase_energy(x, y)
+    flash += len(flash_this_step)
+    for x, y in flash_this_step:
+        octo[x][y] = 0 
+    
+print(flash)
