e211d36bdf141b5a3b87a857a509f344848a768b
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2021-12-22 20:24:59 -0500

    Further simplification

--- 2021/Day 22/puzzle44 int.py
+++ 2021/Day 22/puzzle44 int.py
@@ -3,12 +3,9 @@ with open("input.txt", 'r', encoding='utf-8') as f:
 
 class cube:
     def __init__(self, x1, x2, y1, y2, z1, z2, state):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
-        self.z1 = z1
-        self.z2 = z2
+        self.x1, self.x2 = x1, x2
+        self.y1, self.y2 = y1, y2
+        self.z1, self.z2 = z1, z2
         self.state = state
         
     @property
@@ -27,21 +24,15 @@ for l in lines:
     y1, y2 = map(int, y[2:].split('..'))
     z1, z2 = map(int, z[2:].split('..'))
     
-    x2, y2, z2 = x2 + 1, y2 + 1, z2 + 1
-    
-    this = cube(x1, x2, y1, y2, z1, z2, state)
+    this = cube(x1, x2 + 1, y1, y2 + 1, z1, z2 + 1, state)
     new_cubes = []
     for c in cubes:
         if overlap(this, c):
             new_state = -c.state
-            x1_i = max(this.x1, c.x1)
-            x2_i = min(this.x2, c.x2)
-            y1_i = max(this.y1, c.y1)
-            y2_i = min(this.y2, c.y2)
-            z1_i = max(this.z1, c.z1)
-            z2_i = min(this.z2, c.z2)
-            int_cube = cube(x1_i, x2_i, y1_i, y2_i, z1_i, z2_i, new_state)
-            new_cubes.append(int_cube)
+            x1_i, x2_i = max(this.x1, c.x1), min(this.x2, c.x2)
+            y1_i, y2_i = max(this.y1, c.y1), min(this.y2, c.y2)
+            z1_i, z2_i = max(this.z1, c.z1), min(this.z2, c.z2)
+            new_cubes.append(cube(x1_i, x2_i, y1_i, y2_i, z1_i, z2_i, new_state))
     cubes.append(this)
     cubes.extend(new_cubes)
     
