9a010429f139ca10c1e740e675e44c784497e83d
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2021-12-27 23:07:47 -0500

    Day 5

---
+++ 2017/Day 5/Day 5.py
@@ -0,0 +1,27 @@
+c = [int(x) for x in open('input.txt', 'r', encoding='utf-8')]
+
+pos = 0
+steps = 0
+while pos < len(c):
+    next_pos = pos + c[pos]
+    c[pos] += 1
+    pos = next_pos
+    steps += 1
+
+print("Part 1: " + str(steps))
+
+c = [int(x) for x in open('input.txt', 'r', encoding='utf-8')]
+
+pos = 0
+steps = 0
+while pos < len(c):
+    next_pos = pos + c[pos]
+    c[pos] += 1 if c[pos] < 3 else -1
+    # if c[pos] >= 3:
+    #     c[pos] -= 1
+    # else:
+    #     c[pos] += 1
+    pos = next_pos
+    steps += 1
+
+print("Part 2: " + str(steps))
