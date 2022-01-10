780e84675c66614061bf0ca52153bf25df6cca8b
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2022-01-10 13:50:15 -0500

    Remove unneeded variable

--- 2015/Day 24/puzzle47.py
+++ 2015/Day 24/puzzle47.py
@@ -1,15 +1,12 @@
 from itertools import combinations
 from math import prod
-from sys import maxsize
 
 pkg = [int(l.strip()) for l in open("input.txt", 'r', encoding='utf-8').readlines()]
 
 parts = 3
 size_per = sum(pkg) // parts
 
-min_prod = maxsize
 def hasSum(lst, sub):
-    global min_prod
     for y in range(1, len(lst)):
         for x in (z for z in combinations(lst, y) if sum(z) == size_per):
             if sub == 2:
