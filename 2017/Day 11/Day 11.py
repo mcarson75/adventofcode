b1df5e83bf4d5ac1775236151c9701675c097fba
Matt Carson <54790846+mcarson75@users.noreply.github.com>
2022-01-31 15:17:05 -0500

    Simplified

--- 2017/Day 11/Day 11.py
+++ 2017/Day 11/Day 11.py
@@ -1,32 +1,20 @@
 moves = [l.strip().split(',') for l in open("input.txt", 'r', encoding='utf-8').readlines()][0]
 
-def dist_from_zero(pos):
-    return max([abs(i) for i in pos])
+shifts = {'n': (0, -1, 1),
+          's': (0, 1, -1),
+          'ne': (1, -1, 0),
+          'sw': (-1, 1, 0),
+          'se': (1, 0, -1),
+          'nw': (-1, 0, 1)}
+
+def dist_from_zero(pos): return max([abs(i) for i in pos])
+
+def do_shift(a, b): return tuple([sum(x) for x in zip(a, b)])
 
 part2 = 0
-
 pos = (0, 0, 0)
 for m in moves:
-    q, r, s = pos
-    if m == 'n':
-        s += 1
-        r -= 1
-    elif m == 's':
-        s -= 1
-        r += 1
-    elif m == 'ne':
-        q += 1
-        r -= 1
-    elif m == 'sw':
-        q -= 1
-        r += 1
-    elif m == 'se':
-        q += 1
-        s -= 1
-    elif m == 'nw':
-        q -= 1
-        s += 1
-    pos = (q, r, s)
+    pos = do_shift(pos, shifts[m])
     part2 = max(part2, dist_from_zero(pos))
 
 part1 = dist_from_zero(pos)
