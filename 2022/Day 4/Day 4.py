9a7827dc90454eeb2e13cdd13d9421c4b35b6386
mcarson75 <mcarson75@gmail.com>
2022-12-04 11:45:07 -0500

    Simplified

--- 2022/Day 4/Day 4.py
+++ 2022/Day 4/Day 4.py
@@ -1,7 +1,5 @@
 import re
 
-pattern = r"(\d+)-(\d+),(\d+)-(\d+)"
-
 with open("input.txt", "r", encoding="utf-8") as f:
     lines = [l.strip() for l in f.readlines()]
 
@@ -9,15 +7,14 @@ part1 = 0
 part2 = 0
 
 for line in lines:
-    m = re.match(pattern, line)
+    x1, x2, y1, y2 = [int(i) for i in re.split("-|,", line)]
 
-    x1, x2, y1, y2 = [int(i) for i in list(m.group(1, 2, 3, 4))]
+    r1 = set(range(x1, x2 + 1))
+    r2 = set(range(y1, y2 + 1))
 
-    r1 = range(x1, x2 + 1)
-    r2 = range(y1, y2 + 1)
+    part1 += int(r2.issubset(r1) or r1.issubset(r2))
+    part2 += int(len(r2.intersection(r1)) > 0 or len(r1.intersection(r2)) > 0)
 
-    part1 += int(y1 in r1 and y2 in r1) or (x1 in r2 and x2 in r2)
-    part2 += int(y1 in r1 or y2 in r1) or (x1 in r2 or x2 in r2)
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
