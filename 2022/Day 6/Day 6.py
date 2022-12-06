bc5efa68191d97a4bb590887e45d62c773802fa0
mcarson75 <mcarson75@gmail.com>
2022-12-06 00:18:33 -0500

    Simplify a bit

--- 2022/Day 6/Day 6.py
+++ 2022/Day 6/Day 6.py
@@ -2,10 +2,9 @@ with open("input.txt", "r", encoding="utf-8") as f:
     input = f.read().strip()
 
 
-def find_unique(input, length):
-    for n in range(length, len(input)):
-        check = input[n - length : n]
-        if len(set(check)) == length:
+def find_unique(input, l):
+    for n in range(l, len(input)):
+        if len(set(input[n - l : n])) == l:
             return n
 
 
