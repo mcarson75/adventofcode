aaa60b71e4c688d5e7c92dc9653a3d84593ce042
mcarson75 <mcarson75@gmail.com>
2022-12-25 15:31:08 -0500

    Day 24

--- 2022/Day 24/Day 24.py
+++ 2022/Day 24/Day 24.py
@@ -4,42 +4,35 @@ grid = np.array(
     [list(l.rstrip()) for l in open("input.txt", "r", encoding="utf-8").readlines()]
 )
 
-empty = set(x + y * 1j - 1 - 1j for (y, x) in np.argwhere(grid == "."))
-start = min(empty, key=lambda i: i.imag)
-end = max(empty, key=lambda i: i.imag)
+valid = set((x - 1) + (y - 1) * 1j for (y, x) in np.argwhere(grid != "#"))
+start = min(valid, key=lambda i: i.imag)
+end = max(valid, key=lambda i: i.imag)
 
-WIDTH = int(max(i.real for i in empty - {start, end})) + 1
-HEIGHT = int(max(i.imag for i in empty - {start, end})) + 1
+WIDTH = int(max(i.real for i in valid - {start, end})) + 1
+HEIGHT = int(max(i.imag for i in valid - {start, end})) + 1
 ITERATIONS = int(np.lcm(WIDTH, HEIGHT))
 
 right = [
-    [(x - 1 + t) % WIDTH + (y - 1) * 1j for (y, x) in np.argwhere(grid == ">")]
+    set((x - 1 + t) % WIDTH + (y - 1) * 1j for (y, x) in np.argwhere(grid == ">"))
     for t in range(WIDTH)
 ]
 left = [
-    [(x - 1 - t) % WIDTH + (y - 1) * 1j for (y, x) in np.argwhere(grid == "<")]
+    set((x - 1 - t) % WIDTH + (y - 1) * 1j for (y, x) in np.argwhere(grid == "<"))
     for t in range(WIDTH)
 ]
 up = [
-    [(x - 1) + (y - 1 - t) % HEIGHT * 1j for (y, x) in np.argwhere(grid == "^")]
+    set((x - 1) + (y - 1 - t) % HEIGHT * 1j for (y, x) in np.argwhere(grid == "^"))
     for t in range(HEIGHT)
 ]
 down = [
-    [(x - 1) + (y - 1 + t) % HEIGHT * 1j for (y, x) in np.argwhere(grid == "v")]
+    set((x - 1) + (y - 1 + t) % HEIGHT * 1j for (y, x) in np.argwhere(grid == "v"))
     for t in range(HEIGHT)
 ]
 
-blocked = {}
-for t in range(ITERATIONS):
-    blocked[t] = (
-        set(right[t % WIDTH])
-        | set(left[t % WIDTH])
-        | set(up[t % HEIGHT])
-        | set(down[t % HEIGHT])
-    )
-
-
-grid = blocked[0] | empty
+free = [
+    valid - right[t % WIDTH] - left[t % WIDTH] - up[t % HEIGHT] - down[t % HEIGHT]
+    for t in range(ITERATIONS)
+]
 
 
 def get_best(q, end):
@@ -52,17 +45,18 @@ def get_best(q, end):
             continue
         seen.add((pos, move))
         new_move = move + 1
-
-        for _pos in [pos + 1, pos - 1, pos + 1j, pos - 1j, pos]:
-            if _pos in grid and _pos not in blocked[new_move % ITERATIONS]:
-                q.append((_pos, new_move))
+        _pos = (
+            set([pos + 1, pos - 1, pos + 1j, pos - 1j, pos])
+            & free[new_move % ITERATIONS]
+        )
+        q += [(_p, new_move) for _p in _pos]
 
 
 q = [(start, 0)]
-part1 = get_best(q, end)
+move = part1 = get_best(q, end)
 print(f"Part 1: {part1}")
 
-q = [(end, part1)]
+q = [(end, move)]
 move = get_best(q, start)
 q = [(start, move)]
 part2 = get_best(q, end)
