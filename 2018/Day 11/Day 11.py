a34505a307b1b75adf304b4c51e9cd8e1075454c
mcarson75 <mcarson75@gmail.com>
2023-01-09 17:00:18 -0500

    Alternate day 11

--- 2018/Day 11/Day 11.py
+++ 2018/Day 11/Day 11.py
@@ -10,35 +10,40 @@ def power(x, y, serial):
 
 
 grid = np.zeros((301, 301), dtype=int)
-x = y = best_x = best_y = 1
 for x in range(1, 301):
     for y in range(1, 301):
-        grid[x, y] = power(x, y, serial)
+        grid[x, y] = (
+            power(x, y, serial) + grid[x - 1, y] + grid[x, y - 1] - grid[x - 1, y - 1]
+        )
 
 
 def get_max(size):
     max_total = 0
-    best_x = best_y = 0
+    best_x = best_y = None
     for x in range(1, 301 - size):
         for y in range(1, 301 - size):
-            subgrid = grid[x : x + size, y : y + size]
-            if (total := np.sum(subgrid)) > max_total:
+            total = (
+                grid[x, y]
+                + grid[x + size, y + size]
+                - grid[x, y + size]
+                - grid[x + size, y]
+            )
+            if total > max_total:
                 max_total = total
-                best_x, best_y = x, y
+                best_x, best_y = x + 1, y + 1
 
-    return (best_x, best_y), max_total
+    return best_x, best_y, max_total
 
 
-part1, _ = get_max(3)
-print(f"Part 1: {part1}")
+x1, y1, _ = get_max(3)
 
-best_size = 0
 max_total = 0
 for size in range(1, 301):
-    this, total = get_max(size)
+    _x, _y, total = get_max(size)
     if total > max_total:
         max_total = total
-        part2 = this
+        x2, y2 = _x, _y
         best_size = size
 
-print(f"Part 2: {part2}, {best_size}")
+print(f"Part 1: {(x1,y1)}")
+print(f"Part 2: {(x2,y2,best_size)}")
