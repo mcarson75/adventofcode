11d91d43be87a4a1fe4d3b0b6050227934402f72
mcarson75 <mcarson75@gmail.com>
2023-12-02 15:28:18 -0500

    More simplification

--- 2023/Day 2/Day 2.py
+++ 2023/Day 2/Day 2.py
@@ -1,27 +1,22 @@
-lines = [l.strip() for l in open("input.txt", "r", encoding="utf-8").readlines()]
+lines = [
+    l.strip().split(": ") for l in open("input.txt", "r", encoding="utf-8").readlines()
+]
 
 limits = {"red": 12, "green": 13, "blue": 14}
 part1, part2 = 0, 0
 
-for line in lines:
-    id, draws = line.split(": ")
-    _, id = id.split(" ")
-    id = int(id)
+for id, draws in lines:
+    id = int(id[5:])
+    power = {"red": 0, "green": 0, "blue": 0}
 
-    powers = {"red": 0, "green": 0, "blue": 0}
+    draws = [draw.split() for draw in draws.replace(";", ",").split(", ")]
+    draws = [[color, int(num)] for num, color in draws]
+    for color in power.keys():
+        power[color] = max([num for c, num in draws if c == color])
 
-    subsets = draws.split("; ")
-    for subset in subsets:
-        balls = subset.split(", ")
-        for ball in balls:
-            num, color = ball.split(" ")
-            num = int(num)
-
-            powers[color] = max(num, powers[color])
-
-    if all([powers[color] <= limits[color] for color in limits.keys()]):
+    if all([power[color] <= limits[color] for color in power.keys()]):
         part1 += id
-    part2 += powers["red"] * powers["green"] * powers["blue"]
+    part2 += power["red"] * power["green"] * power["blue"]
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
