b47f150bcaa73a93de12a11e4a6ce61157156b09
mcarson75 <mcarson75@gmail.com>
2023-12-07 18:14:58 -0500

    Further simplification

--- 2023/Day 7/Day 7.py
+++ 2023/Day 7/Day 7.py
@@ -1,57 +1,52 @@
 from functools import cmp_to_key
 
 hands = [l.strip().split(" ") for l in open("input.txt", "r")]
-jokers_wild = False
 card_order = "_23456789TJQKA"
 
 
-def CardRank(c):
-    if c == "J" and jokers_wild:
-        return 0
-    return card_order.index(c)
+def card_rank(c, jokers_wild):
+    return card_order.index(c) if c != "J" or not jokers_wild else 0
 
 
-def HandType(hand):
+def hand_type(hand, jokers_wild):
     cards = {}
     for c in hand:
         cards[c] = cards.get(c, 0) + 1
     if jokers_wild and "J" in cards and hand != "JJJJJ":
         jokers = cards.pop("J")
         cards[max(cards, key=cards.get)] += jokers
-    if max(cards.values()) == 5:
-        return 6
-    if max(cards.values()) == 4:
-        return 5
-    if max(cards.values()) == 3 and len(cards.keys()) == 2:
-        return 4
-    if max(cards.values()) == 3:
-        return 3
-    if max(cards.values()) == 2 and len(cards.keys()) == 3:
-        return 2
-    if max(cards.values()) == 2:
-        return 1
-    return 0
+
+    max_occurrences = max(cards.values())
+    unique_cards = len(cards)
+
+    # Full house
+    if max_occurrences == 3 and unique_cards == 2:
+        return 3.5
+    # Two pair
+    if max_occurrences == 2 and unique_cards == 3:
+        return 2.5
+    # All others
+    return max_occurrences
 
 
-def Hand(a, b):
+def hand(a, b, jokers_wild):
     a, b = a[0], b[0]
-    TypeDiff = HandType(a) - HandType(b)
-    if TypeDiff:
-        return TypeDiff
-    for i in range(len(a)):
-        CardDiff = CardRank(a[i]) - CardRank(b[i])
-        if CardDiff:
-            return CardDiff
-    return 0
+    type_diff = hand_type(a, jokers_wild) - hand_type(b, jokers_wild)
+    if type_diff:
+        return type_diff
+    card_diffs = [
+        card_rank(a[i], jokers_wild) - card_rank(b[i], jokers_wild)
+        for i in range(len(a))
+    ]
+    return next((diff for diff in card_diffs if diff), 0)
 
 
-hands.sort(key=cmp_to_key(Hand))
-part1 = sum([(i + 1) * int(v[1]) for i, v in enumerate(hands)])
+def calculate_score(sorted_hands):
+    return sum([(i + 1) * int(v[1]) for i, v in enumerate(sorted_hands)])
 
-jokers_wild = True
-hands.sort(key=cmp_to_key(Hand))
-part2 = sum([(i + 1) * int(v[1]) for i, v in enumerate(hands)])
 
+part1 = calculate_score(sorted(hands, key=cmp_to_key(lambda a, b: hand(a, b, False))))
+part2 = calculate_score(sorted(hands, key=cmp_to_key(lambda a, b: hand(a, b, True))))
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
