4dd911e285454b924fefcee5d2e9616a0b5c12ea
mcarson75 <mcarson75@gmail.com>
2023-12-25 15:02:00 -0500

    Simplify

--- 2023/Day 25/Day 25.py
+++ 2023/Day 25/Day 25.py
@@ -5,12 +5,7 @@ lines = [
 ]
 
 G = networkx.Graph()
-for src, tgts in lines:
-    for tgt in tgts.split(" "):
-        G.add_edge(src, tgt)
-
-cuts = networkx.minimum_edge_cut(G)
-G.remove_edges_from(cuts)
-rg = list(networkx.connected_components(G))
+[G.add_edge(src, tgt) for src, tgts in lines for tgt in tgts.split(" ")]
+_, rg = networkx.stoer_wagner(G)
 
 print(f"Part 1: {len(rg[0])*len(rg[1])}")
