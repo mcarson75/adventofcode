06c7f6ead97b7d1d285ef0d8eb843a332738755e
mcarson75 <mcarson75@gmail.com>
2024-12-04 09:19:30 -0500

    Simplify

--- 2024/Day 3/Day 3.py
+++ 2024/Day 3/Day 3.py
@@ -1,7 +1,9 @@
 import re
 from math import prod
 
-lines = [l.strip() for l in open("input.txt", "r", encoding="utf-8").readlines()]
+line = "".join(
+    [l.strip() for l in open("input.txt", "r", encoding="utf-8").readlines()]
+)
 
 
 def mul_sum(line):
@@ -9,10 +11,8 @@ def mul_sum(line):
     return sum([prod(int(a) for a in i) for i in re.findall(pattern, line)])
 
 
-part1 = sum([mul_sum(line) for line in lines])
-
-enabled = "".join(lines).split("do()")
-part2 = sum([mul_sum(e.split("don't()")[0]) for e in enabled])
+part1 = mul_sum(line)
+part2 = sum([mul_sum(e.split("don't()")[0]) for e in line.split("do()")])
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
