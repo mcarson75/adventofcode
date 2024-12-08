4512b1bbece29f33d95bc2ca010b439b872f4650
mcarson75 <mcarson75@gmail.com>
2024-12-07 22:13:17 -0500

    Day 6

--- 2024/Day 6/Day 6 refactor.py
+++ 2024/Day 6/Day 6 refactor.py
@@ -7,33 +7,36 @@ grid = np.array(
 obstacles = set(x + y * 1j for (y, x) in np.argwhere(grid == "#"))
 starting_pos = list(x + y * 1j for (y, x) in np.argwhere(grid == "^"))[0]
 dirs = [-1j, 1, 1j, -1]
-dir = 0
 
 MAX_X, MAX_Y = max([i.real for i in obstacles]), max([i.imag for i in obstacles])
 in_bounds = lambda pos: 0 < pos.real < MAX_X and 0 < pos.imag < MAX_Y
+# visited = {}
 obstructions = set()
 
 
-def get_path(pos, obstacle=-1 - 1j):
-    dir = 0
-    visited = {(pos, dir)}
+def get_path(pos, dir, visited, obstacles):
+    initial = len(visited) == 0
+    visited.add((pos, dir))
     while in_bounds(pos):
-        if pos + dirs[dir] not in (obstacles | {obstacle}):
+        if pos + dirs[dir] not in obstacles:
             pos += dirs[dir]
             if (pos, dir) in visited:
-                obstructions.add(obstacle)
-                return True
-            visited.add((pos, dir))
+                return True  # loop
+            elif initial and (
+                get_path(pos, dir, visited.copy(), obstacles | {pos + dirs[dir]})
+            ):
+                obstructions.add(pos + dirs[dir])
         else:
             dir += 1
             dir %= len(dirs)
-    if obstacle == -1 - 1j:
-        return set([x[0] for x in list(visited) if len(x) > 1])
+        visited.add((pos, dir))
+    if initial:
+        return len(set([x[0] for x in list(visited) if len(x) > 1])), len(obstructions)
     return False
 
 
-part1 = len(visited := get_path(starting_pos))
-part2 = sum([get_path(starting_pos, v) for v in visited])
+part1, part2 = get_path(starting_pos, 0, set(), obstacles)
+# part2 = sum([get_path(starting_pos, v) for v in visited])
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
