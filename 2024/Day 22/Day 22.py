a82206d33b4f51c358838a342680feb1a9e62913
mcarson75 <mcarson75@gmail.com>
2024-12-22 12:16:32 -0500

    Simplify

--- 2024/Day 22/Day 22.py
+++ 2024/Day 22/Day 22.py
@@ -2,11 +2,7 @@ from collections import defaultdict
 
 secrets = [int(l.strip()) for l in open("input.txt", "r", encoding="utf-8").readlines()]
 
-
-def mixprune(s, shift):
-    if shift < 0:
-        return (s >> -shift ^ s) & 16777215
-    return (s << shift ^ s) & 16777215
+mixprune = lambda s, shift: ((s >> -shift if shift < 0 else s << shift) ^ s) & 0xFFFFFF
 
 
 def get_secret(s):
@@ -15,18 +11,17 @@ def get_secret(s):
     return s
 
 
-sequences = defaultdict(lambda: 0)
-
-
+sequences = defaultdict(int)
 part1 = 0
+
 for secret in secrets:
     found_sequences = set()
     s = secret
     p1 = secret % 10
     deltas = []
     for n in range(2000):
-        new_secret = get_secret(secret)
-        p2 = new_secret % 10
+        secret = get_secret(secret)
+        p2 = secret % 10
         deltas.append(p2 - p1)
 
         if len(deltas) >= 4:
@@ -37,7 +32,6 @@ for secret in secrets:
                 sequences[k] += p2
 
         p1 = p2
-        secret = new_secret
     part1 += secret
 
 part2 = max(sequences.values())
